b1f844c73915b95574ad42ffaf691cddfb792905
karikEvgeni <ka123123>
2018-02-09 11:54:32 +0200

    extractMetod

--- trelloSeleniumProject/src/test/java/LoginTrelloTest.java
+++ trelloSeleniumProject/src/test/java/LoginTrelloTest.java
@@ -2,16 +2,11 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class LoginTrelloTest {
     FirefoxDriver wd;
@@ -24,22 +19,37 @@ public class LoginTrelloTest {
     
     @Test
     public void LoginTrelloTest() {
-        wd.get("https://trello.com/");
-        wd.findElement(By.xpath("//div[2]")).click();
-        wd.findElement(By.linkText("Log In")).click();
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).clear();
-        wd.findElement(By.id("password")).sendKeys("12345.com");
-        wd.findElement(By.id("user")).click();
-        wd.findElement(By.id("user")).clear();
-        wd.findElement(By.id("user")).sendKeys("elena.telran@yahoo.com");
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).clear();
-        wd.findElement(By.id("password")).sendKeys("12345.com");
+        openSite();
+        clickLogInButton();
+        enterUserName("elena.telran@yahoo.com");
+        enterPassword("12345.com");
+        confirmLogInButton();
+    }
+
+    private void confirmLogInButton() {
         wd.findElement(By.id("login")).click();
     }
-    
+
+    private void enterPassword(String pwd) {
+        wd.findElement(By.id("password")).click();
+        wd.findElement(By.id("password")).clear();
+        wd.findElement(By.id("password")).sendKeys(pwd);
+    }
+
+    private void enterUserName(String userName) {
+        wd.findElement(By.id("user")).click();
+        wd.findElement(By.id("user")).clear();
+        wd.findElement(By.id("user")).sendKeys(userName);
+    }
+
+    private void openSite() {
+        wd.get("https://trello.com/");
+    }
+
+    private void clickLogInButton() {
+        wd.findElement(By.linkText("Log In")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
