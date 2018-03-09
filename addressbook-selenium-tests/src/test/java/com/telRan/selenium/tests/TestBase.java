1e31ff14a1980b15439ee30f527fd1eccb523a6c
karikEvgeni <ka123123>
2018-03-09 11:52:01 +0200

    config_file/properties

--- addressbook-selenium-tests/src/test/java/com/telRan/selenium/tests/TestBase.java
+++ addressbook-selenium-tests/src/test/java/com/telRan/selenium/tests/TestBase.java
@@ -3,18 +3,20 @@ package com.telRan.selenium.tests;
 import com.telRan.selenium.appManager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-    @BeforeClass
+    @BeforeSuite
     public void setUp() throws Exception {
         app.start();
     }
 
-    @AfterClass
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
